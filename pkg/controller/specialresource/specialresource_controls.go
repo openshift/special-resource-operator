b5ee99b4e7f516b56ac7ee53c25870754c6c06d3
Zvonko Kosic <zkosic@redhat.com>
2019-06-21 15:33:16 -0300

    Concat name to 63 characters

--- pkg/controller/specialresource/specialresource_controls.go
+++ pkg/controller/specialresource/specialresource_controls.go
@@ -498,10 +498,7 @@ func Job(n SRO) (ResourceStatus, error) {
 func truncateString(str string, num int) string {
 	bnoden := str
 	if len(str) > num {
-		if num > 3 {
-			num -= 3
-		}
-		bnoden = str[0:num] + "..."
+		bnoden = str[0:num]
 	}
 	return bnoden
 }
