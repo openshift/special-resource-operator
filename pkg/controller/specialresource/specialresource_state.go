533132ab7f54740666308534e676c953c51288ed
Zvonko Kosic <zkosic@redhat.com>
2019-08-29 14:33:14 -0300

    Added dedicated prom state to handle RBAC

--- pkg/controller/specialresource/specialresource_state.go
+++ pkg/controller/specialresource/specialresource_state.go
@@ -82,6 +82,7 @@ func (n *SRO) init(r *ReconcileSpecialResource,
 	addState(n, "/opt/sro/state-driver-validation")
 	addState(n, "/opt/sro/state-device-plugin")
 	addState(n, "/opt/sro/state-device-plugin-validation")
+	addState(n, "/opt/sro/state-prometheus")
 	addState(n, "/opt/sro/state-monitoring")
 	addState(n, "/opt/sro/state-feature-discovery")
 
