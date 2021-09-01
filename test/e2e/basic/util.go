220812990ffd4e5fdf40a3048280865ba212ab59
Pablo Acevedo Montserrat <pabloacevedom@gmail.com>
2021-09-01 20:28:17 +0200

    Fix simple-kmod e2e test to disable debug in not ready nodes (#89)
    Co-authored-by: Pablo Acevedo Montserrat <pablo.acevedo.m@gmail.com>

--- test/e2e/basic/util.go
+++ test/e2e/basic/util.go
@@ -286,3 +286,17 @@ func WaitForClusterOperatorCondition(cs *framework.ClientSet, interval, duration
 	}
 	return nil
 }
+
+// IsNodeReady helps determine if a given node is ready or not.
+func IsNodeReady(node corev1.Node) bool {
+	found := false
+	for _, condition := range node.Status.Conditions {
+		if condition.Type == corev1.NodeReady {
+			found = true
+			if condition.Status != corev1.ConditionTrue {
+				return false
+			}
+		}
+	}
+	return found
+}
