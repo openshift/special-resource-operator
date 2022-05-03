a365c1703c4b24f7200919b75235011bb086f8a8
Yevgeny Shnaidman <60741237+yevgeny-shnaidman@users.noreply.github.com>
2022-05-03 11:12:32 +0300

    MGMT-10184: Remove special-resource-lifecycle configmap usage (#178)
    special-resource-lifecycle configmap is used by SRO to monitor the pods of the Daemonsets.
    The main use is for DaemonSets with updateStrategy set "onDelete".
    With "onDelete" strategy, upon DS update no new pods are created until the old one are deleted externally
    (by user or some other code).
    In ForDaemonSet function in poll package, we have 3 stages of polling:
    1) Check that DS exists
    2) in case of onDelete, check DS pods are not present in configmap (meaning they were deleted
    3) wait for DS desired number of pods to equal numberAvailble
    
    This PR will remove the configmap, and now will check that all the pods were
    deleted by checking that DS generation label and pods generation template label are equal
    (meaning that only the new pods are running)

--- pkg/lifecycle/lifecycle_test.go
+++ pkg/lifecycle/lifecycle_test.go
@@ -2,7 +2,6 @@ package lifecycle_test
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -15,7 +14,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -134,65 +132,5 @@ var _ = Describe("GetPodFromDeployment", func() {
 			GetPodFromDeployment(context.Background(), types.NamespacedName{Namespace: namespace, Name: name})
 
 		Expect(pl.Items).To(HaveLen(nPod))
-	})
-})
-
-var _ = Describe("UpdateDaemonSetPods", func() {
-	const namespaceEnvVar = "OPERATOR_NAMESPACE"
-
-	AfterEach(func() {
-		err := os.Unsetenv(namespaceEnvVar)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("should update the ConfigMap", func() {
-		err := os.Setenv(namespaceEnvVar, namespace)
-		Expect(err).NotTo(HaveOccurred())
-
-		nsn := types.NamespacedName{
-			Namespace: namespace,
-			Name:      name,
-		}
-
-		cmNsn := types.NamespacedName{
-			Namespace: namespace,
-			Name:      "special-resource-lifecycle",
-		}
-
-		gomock.InOrder(
-			mockClient.EXPECT().
-				Get(context.Background(), nsn, &appsv1.DaemonSet{}).
-				Do(func(_ context.Context, key types.NamespacedName, ds *appsv1.DaemonSet) {
-					ds.Spec.Selector = &metav1.LabelSelector{MatchLabels: labels}
-					ds.SetNamespace(key.Namespace)
-				}),
-			mockClient.EXPECT().
-				List(context.Background(), &v1.PodList{}, optNs, optLabels).
-				Do(func(_ context.Context, pl *v1.PodList, _, _ client.ListOption) {
-					pl.Items = []v1.Pod{
-						{
-							ObjectMeta: metav1.ObjectMeta{
-								Name:      "pod1",
-								Namespace: namespace,
-							},
-						},
-						{
-							ObjectMeta: metav1.ObjectMeta{
-								Name:      "pod2",
-								Namespace: namespace,
-							},
-						},
-					}
-				}),
-			mockStorage.EXPECT().UpdateConfigMapEntry(context.Background(), "39005a809548c688", "*v1.Pod", cmNsn),
-			mockStorage.EXPECT().UpdateConfigMapEntry(context.Background(), "39005d809548cba1", "*v1.Pod", cmNsn),
-		)
-
-		obj := unstructured.Unstructured{}
-		obj.SetNamespace(namespace)
-		obj.SetName(name)
-
-		err = lifecycle.New(mockClient, mockStorage).UpdateDaemonSetPods(context.Background(), &obj)
-		Expect(err).NotTo(HaveOccurred())
 	})
 })
